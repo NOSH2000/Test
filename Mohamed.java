4b9e96a99fdbbd6f370fdbfdcfd49dd8543248a6
0Rooot0 <74662621+0Rooot0@users.noreply.github.com>
2021-05-03 17:12:15 -0400

    Add files via upload

---
+++ Mohamed.java
@@ -0,0 +1,14 @@
+package QueensCollege;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+class Solution {
+    public int[] runningSum(int[] nums) {
+
+    }
+}
+
